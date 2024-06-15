9bf28aebfb1007ccc7243e8aba1927f8a70931f7
Allan Saddi <allan@saddi.com>
2024-06-15 15:43:38 -0700

    llamacpp: Add simple wrapper script.

---
+++ llamacpp/llama.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+BASE=$(dirname "$0")
+
+if [ $# -lt 1 -o "$1" = "--help" -o "$1" = "-h" ]; then
+  {
+    echo "Usage: $0 <command> [args...]"
+    echo
+    echo "Available commands:"
+    cd $BASE
+    for fn in llama-*; do
+      cmd=$(echo "$fn" | cut -c 7-)
+      echo "  $cmd"
+    done
+    exit 1
+  } >&2
+fi
+
+cmd=$1
+
+shift
+
+if [ ! -x $BASE/llama-$cmd ]; then
+  echo "$0: Unknown command '$cmd'" >&2
+  exit 2
+fi
+
+exec $BASE/llama-$cmd "$@"
