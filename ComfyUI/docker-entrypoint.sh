bf45e2039be5fe0d3c893b6451abf5c3f555c521
Allan Saddi <allan@saddi.com>
2024-09-30 20:54:32 -0700

    ComfyUI: Reconfigure rather than symlink.

--- ComfyUI/docker-entrypoint.sh
+++ ComfyUI/docker-entrypoint.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 BASE="$(realpath "$(dirname "$0")")"
 
-mkdir -p /data/input /data/output
-
-# Move models and custom_nodes directories to /data, if needed
-for dir_name in models custom_nodes; do
-    # Copy the directory over
-    if [ ! -d /data/$dir_name ]; then
-        tar -cf - $dir_name | tar -C /data -xf -
-    fi
-
-    # And replace it with a symlink
-    mv $dir_name $dir_name.old
-    ln -s /data/$dir_name
-done
+# Ensure appropriate directories exist in the data volume
+mkdir -p /data/input /data/output /data/models/checkpoints \
+  /data/models/clip /data/models/clip_vision /data/models/configs \
+  /data/models/controlnet /data/models/embeddings /data/models/loras \
+  /data/models/upscale_models /data/models/vae /data/custom_nodes
 
 if [ "$1" = "shell" ]; then
     shift
