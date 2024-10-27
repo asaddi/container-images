3905f4b90393009b2dc1d088638c13e5559eed7f
Allan Saddi <allan@saddi.com>
2024-10-27 14:03:34 -0700

    Forgot to add extra path for unet

--- comfyui/docker-entrypoint.sh
+++ comfyui/docker-entrypoint.sh
@@ -5,6 +5,7 @@ BASE="$(realpath "$(dirname "$0")")"
 mkdir -p /data/user /data/input /data/output /data/models/checkpoints \
   /data/models/clip /data/models/clip_vision /data/models/configs \
   /data/models/controlnet /data/models/embeddings /data/models/loras \
+  /data/models/unet \
   /data/models/upscale_models /data/models/vae /data/custom_nodes
 
 if [ "$1" = "shell" ]; then
